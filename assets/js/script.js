a0956930a953ac511206c791b94c017996673acd
CharissaHollister <charissa.hollister@diasorin.com>
2022-05-16 20:41:45 -0500

    appending elements js draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,198 @@
 
 // //---------------Add event listener-----------
 
+// //display repo content containers
+var currentContainerEl = document.querySelector(".day1container");
+var futureContainerEl = document.querySelector(".futureContainer");
+
+var citySearchTerm = document.querySelector("#city-search-term");
+var city = citySearchTerm;
+var currentDayDate = document.querySelector("#day1date");
+var currentDayInfo = document.querySelector("#day1Info");
+
+// container is futuredayInfo for 5 day forecast section
+var futureContainer = document.querySelector(".futureContainer")
+/////clear out cards before displaying for a new city
+
+var displayCityToday = function(data, i){
+//location being displayed
+var place = data.location.name;
+// current day weather data 
+var currentDate = data.forecast.forecastday[i].date;
+var currentIcon = data.forecast.forecastday[i].day.condition.icon;
+var currentIconAltText = data.forecast.forecastday[i].day.condition.text;
+var currentTemp = data.forecast.forecastday[i].day.avgtemp_f;
+var currentWind = data.forecast.forecastday[i].day.maxwind_mpph;
+var currentHum = data.forecast.forecastday[i].day.avghumidity;
+//console.log(place, currentDate, currentHum, currentWind, currentIcon, currentTemp, currentIconAltText)
+};
+
+var displayCityFuture = function(data, i){
+     // clear old content
+ futureContainerEl.textContent = "";
+ citySearchTerm.textContent = "";
+    //location being displayed
+    var place = data.location.name;
+    // current day weather data 
+    var currentDate = data.forecast.forecastday[i].date;
+    var currentIcon = data.forecast.forecastday[i].day.condition.icon;
+    var currentIconAltText = data.forecast.forecastday[i].day.condition.text;
+    var currentTemp = data.forecast.forecastday[i].day.avgtemp_f;
+    var currentWind = data.forecast.forecastday[i].day.maxwind_mpph;
+    var currentHum = data.forecast.forecastday[i].day.avghumidity;
+    //console.log(currentDate, currentHum, currentIcon, currentTemp, currentIconAltText)
+    
+//create: <div class="col-auto card card-body bg-info"></div>
+var dayCardEl = document.createElement("div");
+dayCardEl.classList = "col-auto card card-body bg-info";
+//create: <h5 class="card-header-date" id="day2date"></h5>
+var daydateEl = document.createElement("h5");
+daydateEl.classList = "card-header-date";
+//create: <div class="card-body" id="dayInfo"></div>
+var dayCardBodyEl = document.createElement("div");
+dayCardBodyEl.classList = "card-body";
+//create: <p class="infoLines"></p>
+var dayTempEl = document.createElement("p");
+dayTempEl.classList = "card-info";
+var dayIconEl = document.createElement("p");
+dayIconEl.classList = "card-info";
+//dayIconEl.setAttribute("href", "alt:'currentIconAltText'");
+var dayWindEl = document.createElement("p");
+dayWindEl.classList = "card-info";
+var dayHumEl = document.createElement("p");
+dayHumEl.classList = "card-info";
+
+
+//create card blocks
+futureContainer.appendChild(dayCardEl);
+dayCardEl.appendChild(daydateEl);
+dayCardEl.appendChild(dayCardBodyEl);
+dayCardBodyEl.appendChild(dayTempEl);
+dayCardBodyEl.appendChild(dayIconEl);
+dayCardBodyEl.appendChild(dayWindEl);
+dayCardBodyEl.appendChild(dayHumEl);
+
+//fill in card information
+daydateEl.textContent = "date"
+dayTempEl.textContent = "temp"
+dayIconEl.textContent = "icon"
+dayWindEl.textContent = "wind"
+dayHumEl.textContent = "hum"
+
+
+};
+
+// //function to get city info
+var getLocationInfo = function (city) {
+  // format the api url
+    var apiUrl = "http://api.weatherapi.com/v1/forecast.json?key=55eec00c163d4c31961194524221605&q=" + city +"&days=10&aqi=no&alerts=yes";
+    // make a request to the url
+    fetch(apiUrl)
+        .then(function (response) {
+            // request was successful
+            if (response.ok) {
+                console.log(response)
+                response.json().then(function (data) {
+                    console.log(data)
+                    for (var i = 1; i < data.forecast.forecastday.length; i++){
+                        if(i=0){displayCityToday(data, i);}
+                        else{ displayCityFuture(data, i);
+
+                        }
+            } });
+            } else {
+                alert("Error: city Not Found");
+            }
+        })
+        .catch(function (error) {
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        alert("Unable to connect to weather api");
+        });
+};
+
+getLocationInfo("New York");
+
+
+
+
+
+
+                    //  var card = `<div class="col-auto card card-body bg-info " id="day2">
+                    //     <h5 class="card-header-date" id="daydate">${data.forecast.forecastday[i].date}</h5>
+                    //     <div class="card-body" id="dayInfo">
+                    //     </div>
+                    //     </div>`
+                    //     futureContainer.innerHTML = card;
+//     var dayTempEl = document.createElement("p");
+//     dayTempEl.classList = "card-info flex-row justify-space-between align-center";
+
+//     dayTempEl.textContent = currentTemp;
+
+// futuredayInfo.appendChild(dayTempEl)
+
+
+    // var futuredayDate = document.querySelector("#day" + i +"date");
+    // var futuredayInfo = document.querySelector("#day" + i +"Info");
+    // futuredayDate.textContent = currentDate;
+
+
+
+                            
+                            
+// ${data.forecast.forecastday[i].date}
+// //display user repos
+// var displayRepos = function (data, i) {
+//   console.log(repos);
+//   console.log(searchTerm);
+//   // clear old content
+//   repoContainerEl.textContent = "";
+//   repoSearchTerm.textContent = searchTerm;
+//   // check if api returned any repos
+//   if (repos.length === 0) {
+//     repoContainerEl.textContent = "No repositories found.";
+//     return;
+//   }
+//   // loop over repos
+//   for (var i = 0; i < repos.length; i++) {
+//     // format repo name
+//     var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+    // create a container for each repo
+    // create a link for each repo
+    // var repoEl = document.createElement("a");
+    // repoEl.classList = "list-item flex-row justify-space-between align-center";
+    // repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+
+    // // create a span element to hold repository name
+    // var titleEl = document.createElement("span");
+    // titleEl.textContent = repoName;
+
+    // // append to container
+    // repoEl.appendChild(titleEl);
+
+    // // create a status element
+    // var statusEl = document.createElement("span");
+    // statusEl.classList = "flex-row align-center";
+
+    // check if current repo has issues or not
+//     if (repos[i].open_issues_count > 0) {
+//       statusEl.innerHTML =
+//         "<i class='fas fa-times status-icon icon-danger'></i>" +
+//         repos[i].open_issues_count +
+//         " issue(s)";
+//     } else {
+//       statusEl.innerHTML =
+//         "<i class='fas fa-check-square status-icon icon-success'></i>";
+//     }
+
+//     // append to container
+//     repoEl.appendChild(statusEl);
+
+//     // append container to the dom
+//     repoContainerEl.appendChild(repoEl);
+//   }
+// };
+
 
 // //////from git er done assignment
 // //form variables
@@ -35,74 +227,17 @@
 //   }
 //   console.log(event);
 // };
+//////add recent searches from local storage to the city buttons
+/* <div id="city-buttons" class="card-body">
+<button class="btn" data="data-city1">New York</button>
+<!-- add search history cities  --> */
 
-
-
-
-
-
-var displayCityToday = function(data, citySearchTerm, i){
-var testCityPlacement = document.querySelector(".card-header-date");
-//location being displayed
-var place = data.location.name;
-// current day weather data 
-var currentDate = data.forecast.forecastday[i].date;
-var currentIcon = data.forecast.forecastday[i].day.condition.icon;
-var currentIconAltText = data.forecast.forecastday[i].day.condition.text;
-var currentTemp = data.forecast.forecastday[i].day.avgtemp_f;
-var currentHum = data.forecast.forecastday[i].day.avghumidity;
-console.log(currentDate, currentHum, currentIcon, currentTemp, currentIconAltText)
-};
-
-var displayCityFuture = function(data, citySearchTerm, i){
-    var testCityPlacement = document.querySelector(".card-header-date");
-    //location being displayed
-    var place = data.location.name;
-    // current day weather data 
-    var currentDate = data.forecast.forecastday[i].date;
-    var currentIcon = data.forecast.forecastday[i].day.condition.icon;
-    var currentIconAltText = data.forecast.forecastday[i].day.condition.text;
-    var currentTemp = data.forecast.forecastday[i].day.avgtemp_f;
-    var currentHum = data.forecast.forecastday[i].day.avghumidity;
-    console.log(currentDate, currentHum, currentIcon, currentTemp, currentIconAltText)
-    };
-
-// //function to get city info
-var getLocationInfo = function (city) {
-  // format the api url
-    var apiUrl = "http://api.weatherapi.com/v1/forecast.json?key=55eec00c163d4c31961194524221605&q=" + city +"&days=10&aqi=no&alerts=yes";
-    // make a request to the url
-    fetch(apiUrl)
-        .then(function (response) {
-        // request was successful
-        if (response.ok) {
-            console.log(response)
-            response.json().then(function (data) {
-                console.log(data)
-                for (var i = 0; i < data.forecast.forecastday.length; i++){
-                    if(i=0){displayCityToday(data, city, i);}
-            else{ displayCityFuture(data, city, i);}
-        } });
-        } else {
-            alert("Error: city Not Found");
-        }
-        })
-        .catch(function (error) {
-        // Notice this `.catch()` getting chained onto the end of the `.then()` method
-        alert("Unable to connect to weather api");
-        });
-
-};
-
-getLocationInfo("New York");
 
 
 // //submit button
 // userFormEl.addEventListener("submit", formSubmitHandler);
 
-// //display repo content containers
-var repoContainerEl = document.querySelector("#repos-container");
-var citySearchTerm = document.querySelector("#city-search-term");
+
 
 // //display user repos
 // var displayRepos = function (repos, searchTerm) {
